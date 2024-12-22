007351fd13aee6d4487636c1a35c6c54d9d3d11b
ansuman-qa <ansu2qa@gmail.com>
2024-12-22 23:46:42 +0530

    add all

---
+++ src/test/java/com/example/devops/demo/devops/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.devops.demo.devops;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		System.out.print("Run First test");
+	}
+
+}
---
+++ src/test/java/com/example/devops/demo/devops/secondTest.java
@@ -0,0 +1,18 @@
+package com.example.devops.demo.devops;
+
+import org.testng.annotations.Test;
+
+public class secondTest {
+	
+	@Test
+	public void sampleTwo() {
+		System.out.println("Run second Test");
+	}
+	
+	@Test
+	public void sampleThree() {
+		
+		System.out.println("Run Third test");
+	}
+
+}
